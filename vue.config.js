c0e46d1286fea1a3b318622178a0f925bb8189fa
Andrew Zhang <113492071+alockinalock@users.noreply.github.com>
2024-02-28 23:18:58 -0600

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Personal-Website/' : '/',
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/Personal-Website/" : "/",
+})
