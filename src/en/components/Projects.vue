70284a2a941e05fd1c0ab5002b1ebbc99ae98223
alockinalock <kanjigawa@gmail.com>
2024-03-15 23:07:09 -0500

    expanding gallery

--- src/en/components/Projects.vue
+++ src/en/components/Projects.vue
@@ -3,9 +3,51 @@
 </script>
 
 <template>
-  
+  <div class="items-container">
+
+    <div class="item">
+      <h1>test2</h1>
+    </div>
+
+    <div class="item">
+      <h1>test2</h1>
+    </div>
+
+    <div class="item">
+      <h1>test2</h1>
+    </div>
+
+    <div class="item">
+      <h1>test2</h1>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
+.item:hover {
+  width: 12em;
+}
+.item {
+  margin-left: 50px;
+  width: 8em;
 
+  background-color: rgb(60, 60, 60);
+
+  transition: width .25s ease, right .25s ease;
+}
+.items-container {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+  height: 75%;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
 </style>
