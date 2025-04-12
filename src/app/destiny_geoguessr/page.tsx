88f0c3640c9184b285da77ffe92311edc6205fb8
Andrew Zhang <113492071+alockinalock@users.noreply.github.com>
2025-04-12 04:12:19 -0500

    added destiny geoguessr project to projects

---
+++ src/app/destiny_geoguessr/page.tsx
@@ -0,0 +1,64 @@
+import styles from "./destiny_geoguessr.module.css";
+import Image from "next/image";
+
+export default function destiny_geoguessr() {
+        return (
+                <div className="mainWrapper">
+                        <main>
+                            <div className={styles.subMainWrapper}>
+                                <h1 className={styles.title} >
+                                        <span>
+                                                <a href = "/" className={styles.backtrackTitle}>alockinalock&nbsp;</a>
+                                                / 
+                                                <a href = "/projects" className={styles.backtrackTitle}>projects&nbsp;</a> 
+                                                / destiny_geoguessr
+                                        </span>
+                                </h1>
+                                <div style = { { margin: "0 21.44px", display: "flex", flexDirection: "column"}}>
+                                  
+                                  <div style = {{ height: "20px" }}></div>
+
+                                  <span>A rendition of the Geoguessr formula but for the video game Destiny 2. Inspired by <a href="https://valoguessr.com/" style = {{ fontStyle: "italic", textDecoration: "underline"}}>Valoguessr</a>.</span>
+                                  <span style = {{opacity: "0.4"}}>In active development.</span>
+
+                                  <div style={{ height: "10px" }}></div>
+
+                                  <div className={styles.buttonGroup}>
+                                        <a href="https://github.com/alockinalock/destiny-geoguessr" 
+                                        className={styles.bigLink}>
+                                                <div className={styles.bigLinkBackground}></div>
+                                                GitHub
+                                        </a>
+                                  </div>
+
+                                  <div style = {{ height: "30px" }}></div>
+                                  
+                                  <h3>Screenshots</h3>
+
+                                  <Image 
+                                  alt="The config page for Destiny Geoguessr which allows users to configure game settings"
+                                  src="/dgcp.png"
+                                  width={1341}
+                                  height={791}
+                                  style={{ maxWidth: "100vmin", height: "100%"}}
+                                  />
+                                  <span style = {{opacity: "0.4", margin: "8px 0"}}>This image shows the [unstyled] configuration page.</span>
+
+                                  <Image 
+                                  alt="The geoguessr page for Destiny Geoguessr which users play in"
+                                  src="/dggp.png"
+                                  width={1341}
+                                  height={791}
+                                  style={{ maxWidth: "100vmin", height: "100%"}}
+                                  />
+                                  <span style = {{opacity: "0.4",  margin: "8px 0"}}>This image shows the [unstyled] geoguessr page a.k.a. the game itself.</span>
+
+                                  <div style={{ height: "120px" }}></div>
+                                  
+                                </div>
+
+                            </div>
+                        </main>  
+                </div>
+        );
+}
