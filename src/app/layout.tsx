4d13e32019813457861fcbf8fd7b1b793ba76353
Andrew Zhang <113492071+alockinalock@users.noreply.github.com>
2025-04-23 21:12:06 -0500

    meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="images/favicon.ico" sizes="any"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         {children}
